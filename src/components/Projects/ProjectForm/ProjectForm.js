112ecaa06338f7263d6d4a47bb823d6b7ca6e5db
sakshidheer <69763547+sakshidheer@users.noreply.github.com>
2022-01-23 02:07:32 +0530

    Adding theme in new project window

--- src/components/Projects/ProjectForm/ProjectForm.js
+++ src/components/Projects/ProjectForm/ProjectForm.js
@@ -1,14 +1,37 @@
+import { TextField } from '@material-ui/core';
+import { useState } from 'react';
 import classes from './ProjectForm.module.css';
+import { CirclePicker } from 'react-color';
+import { FormLabel } from '@material-ui/core';
 
-const ProjectForm = () =>{
-return (
-    <div className={classes.container}>
-        <div className={classes.form}>
-        <h4>Add New Project</h4>
+const ProjectForm = () => {
+    let [name, setName] = useState(null);
+    let [isNameValid, setIsNameValid] = useState(true);
+    return (
+        <div className={classes.container}>
+            <div className={classes.form}>
+                <div>Add New Project</div>
+                <TextField
+                    id="name"
+                    label="Name"
+                    multiline
+                    rowsMax={4}
+                    value={name}
+                    error={!isNameValid}
+                    fullWidth
+                    margin="normal"
+                    variant="outlined"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <FormLabel>Theme</FormLabel>
+                <CirclePicker triangle="hide"/>
+            </div>
+
         </div>
-        
-    </div>
-)
+    )
 }
 
 export default ProjectForm;
