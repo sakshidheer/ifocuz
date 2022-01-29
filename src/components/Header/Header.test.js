1494d60723d305c67533efa8c11bf641513fa8e6
sakshidheer <69763547+sakshidheer@users.noreply.github.com>
2022-01-30 02:55:02 +0530

    First successful test case

--- src/components/Header/Header.test.js
+++
@@ -1,7 +0,0 @@
-import Header from './Header';
-import { render, screen } from '@testing-library/react'
-
-test('check if header is rendered correctly', () => {
-    render(<Header />);
-    
-})
--- src/components/Task/TaskForm/Labels/Labels.test.js
+++ src/components/Task/TaskForm/Labels/Labels.test.js
@@ -1,1 +1,22 @@
+import { render, screen } from "@testing-library/react"
+import Labels from "./Labels"
 
+test("Label render",()=>{
+    let labels = [{
+        value:'1',
+        displayName: 'One'
+    },{
+        value:'2',
+        displayName: 'Two'
+    },
+    {
+        value:'3',
+        displayName: 'Three'
+    }];
+
+    const onChange = jest.fn;
+    render(<Labels labels={labels} setLabel={onChange}/>)
+    
+    const options = screen.getAllByRole('option');
+    expect(options.length).toBe(3);
+})
